dfd0a99aa31c8b28bdb52d4f405e42106ba83dc9
cisse410 <dramisse90@gmail.com>
2024-03-08 03:02:48 +0000

    changes

--- bin/app.dart
+++ bin/app.dart
@@ -4,13 +4,14 @@ import 'dart:io';
 
 void main(List<String> arguments) {
   print("Dart user input: ");
-  print("Veuillez saisir votre nom: ");
-  String? name = stdin.readLineSync();
-  print("Bonjour $name : ");
-  print("$name quel est votre age: ");
-  int? age = int.parse(stdin.readLineSync()!);
-  print("Ah $name  vous $age ans ! ");
-  print("----------------°°°-----------------");
+  // print("Veuillez saisir votre nom: ");
+  // String? name = stdin.readLineSync();
+  // print("Bonjour $name : ");
+  // print("$name quel est votre age: ");
+  // int? age = int.parse(stdin.readLineSync()!);
+  // print("Ah $name  vous $age ans ! ");
+  print("\t\t\t\t\t============== CISSE410 ===================\n\n");
+  // print("----------------°°°-----------------");
   print(
       "This program is a simple example to do some pratics of dart basic's. This consists that do ask a user a two number and on operation");
   print("Veuillez saisir un nombre: ");
@@ -21,7 +22,37 @@ void main(List<String> arguments) {
   print("1 : Addition, 2 : Soustraction, 3 : Multiplication, 4 : Division");
   int? choice = int.parse(stdin.readLineSync()!);
   if (choice == 1) {
-    print("Vous choisi de faire l'addition");
-    print("Voici le resultat de ${number + number2}");
+  } else if (choice == 2) {
+    print("Vous choisi de faire la soustraction");
+    print("Voici le resultat de ${number - number2}");
+  } else if (choice == 3) {
+    print("Vous choisi de faire la multiplication");
+    print("Voici le resultat de ${number * number2}");
+  } else if (choice == 4) {
+    print("Vous choisi de faire la division");
+    print("Voici le resultat de ${number / number2}");
+  } else {
+    print("Veuillez revoir votre choix :)");
+  }
+
+  switch (choice) {
+    case 1:
+      print("Vous choisi de faire l'addition");
+      print("Voici le resultat de ${number + number2}");
+      break;
+    case 2:
+      print("Vous choisi de faire la soustraction");
+      print("Voici le resultat de ${number - number2}");
+      break;
+    case 3:
+      print("Vous choisi de faire la multiplication");
+      print(
+          "Voici le resultat de la multiplication de $number par $number2 ${number * number2}");
+      break;
+    case 4:
+      print("Vous choisi de faire la division");
+      print(
+          "Voici le resultat de la division de $number par $number2 : ${number / number2}");
+    default:
   }
 }
